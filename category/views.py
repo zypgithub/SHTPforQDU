f00b03ae75b95f65ac71a3251de2e98cd5682c79
zyp <276291420@qq.com>
2014-06-27 11:37:44 +0800

    增加了一个显示类别的组件

--- category/views.py
+++ category/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# coding:utf-8
+from category import category
+from shortcuts import render
+
+def categories_list(request):
+    category_list = category.objects.all().order_by('-created_at')
+    return (render(
+            request,
+            'category/categories_list.html',
+            {'categories_list': category_list})
+    )
