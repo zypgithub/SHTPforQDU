2553f0bc6a2c9f33acd241cf22abff3d1ae6b67a
WangFushu <wangfushu1992@gmail.com>
2014-06-29 10:32:28 +0800

    添加创建用户时的IntegrityError异常捕获

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 #coding:utf-8
 from base64 import b64encode
+import json
 
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -24,17 +26,28 @@ def register(request):
         school_id = request.POST.get("school_id")
         jw_password = request.POST.get("jw_password")
         captcha = request.POST.get("captcha")
-        nickname = request.POST.get("nickname")
         password = request.POST.get("password")
+        print school_id + jw_password + " " + captcha + password
         user_info = get_user_info(request, school_id, jw_password, captcha)
         status = user_info[-1]
         if status[0] == 200:
             #TODO:捕获异常
-            user = User(username=school_id)
-            user.set_password(password)
-            user.save()
+            try:
+                user = User(username=school_id)
+                user.set_password(password)
+                user.save()
+            except IntegrityError:
+                response = {'status_code': 606,
+                            'error_info': '用户已存在'
+                           }
+                return HttpResponse(json.dumps(response))
             return render(request, 'users/user_info.html', {'user_info': user_info})
+        else:
         #TODO: 错误状态的响应
+            response = {'status_code': status[0],
+                        'error_info' : status[1]
+                       }
+            #return HttpResponse(json.dumps(response))
         return HttpResponse(status[1], content_type="text/html;charset=utf-8")
 
 def save_user(request):
