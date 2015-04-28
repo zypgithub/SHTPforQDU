e5edcdd40cded947abf8d6fd071b6c13429290bc
zypgithub <276291420@qq.com>
2015-04-28 04:18:55 -0700

    finish goods modeling

--- goods/models.py
+++ goods/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from category.models import category
 from django.contrib.auth.models import User
 
+
 class goods(models.Model):
     category = models.ForeignKey(category)
     title = models.CharField(max_length=100, null=False, blank=False)
@@ -11,4 +12,10 @@ class goods(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     browse_count = models.IntegerField()
     alter_at = models.DateTimeField(auto_now=True)
-    
+    goods_cover = models.ImageField(upload_to="Image")
+
+class photo(models.Model):
+    photo = models.ImageField(upload_to="Image")
+    goods = models.ForeignKey(goods)
+    upload_time = models.DateTimeField(auto_now_add=True)
+
