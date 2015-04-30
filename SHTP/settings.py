b6aa07f873fa654e93c08fa757531074a5d1d3f6
ZhangYanpeng <276291420@qq.com>
2015-05-01 01:36:38 +0200

    complate the function of displaying picture

--- SHTP/settings.py
+++ SHTP/settings.py
@@ -51,7 +51,9 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '../')
+SITE_ROOT = PROJECT_ROOT
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'Image') 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
--- SHTP/urls.py
+++ SHTP/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -27,5 +29,5 @@ urlpatterns = patterns('',
     url(r'^goods/(?P<filter_category>\d+)/', 'goods.views.list_goods', name='goods_list'),
 
 #    url(r'^Image/(?P<image_name>.*)', 'goods.views.get_image', name='get_image'),
-#    url(r'^Image/(?P<path>.*)', 'django.views.static.serve', {'document_root': '/Image/'})
-)
+    #url(r'^Image/(?P<path>.*)', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
